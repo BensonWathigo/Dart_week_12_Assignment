a32d3980c0b14a427b43482c1a1360ba03449641
BensonWathigo <bensonwathigo21@gmail.com>
2024-06-07 18:53:32 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/week_12_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.week_12_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/splash_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'login_page.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToLogin();
+  }
+
+  _navigateToLogin() async {
+    await Future.delayed(Duration(seconds: 3), () {});
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => LoginPage()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset(
+            'Images/funny-crazy-cartoon.jpg'), // Ensure you have a logo image in assets
+      ),
+    );
+  }
+}
